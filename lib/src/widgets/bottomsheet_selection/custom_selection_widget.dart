69675d604221f392c5cdaead0f32b4458b1e2286
Ömer Burak Taşkın <taskinburak2014@gmail.com>
2026-04-10 01:03:16 +0300

    custom selection widget updated

--- lib/src/widgets/bottomsheet_selection/custom_selection_widget.dart
+++ lib/src/widgets/bottomsheet_selection/custom_selection_widget.dart
@@ -4,42 +4,52 @@ import 'package:flutter_project_core/src/extensions/widget_extensions.dart';
 import 'package:flutter_project_core/src/widgets/bottomsheet_selection/selection_bottom_sheet.dart';
 
 class CustomSelectionWidget<T> extends StatelessWidget {
-  const CustomSelectionWidget({
-    super.key,
-    required this.label,
-    required this.onSelected,
-    required this.hintText,
-    required this.items,
-    this.selectedItem,
-    this.isActive = true,
-    this.bottomSheetTitle,
-  });
-  final String label;
-  final String hintText;
+  const CustomSelectionWidget(
+      {super.key,
+      this.label,
+      this.hintText,
+      required this.onSelected,
+      required this.items,
+      this.selectedItem,
+      this.isActive = true,
+      this.bottomSheetTitle,
+      this.borderRadius = 16,
+      this.customArrow,
+      this.borderColor,
+      this.width});
+  final String? label;
+  final String? hintText;
   final bool isActive;
   final Function(T) onSelected;
   final List<T> items;
   final T? selectedItem;
   final String? bottomSheetTitle;
+  final double? width;
+  final Color? borderColor;
+  final double borderRadius;
+  final Widget? customArrow;
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       spacing: 8,
       children: [
-        Text(label),
+        if (label != null) Text(label!),
         Container(
-          width: double.infinity,
+          width: width,
           height: 48,
           padding: EdgeInsets.symmetric(horizontal: 16),
           decoration: BoxDecoration(
             color: Colors.grey[50],
             borderRadius: BorderRadius.circular(16),
-            border: Border.all(color: Colors.grey[300]!),
+            border: Border.all(color: borderColor ?? Colors.grey[300]!),
           ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [Text(hintText), Icon(Icons.arrow_drop_down_rounded)],
+            children: [
+              if (hintText != null) Text(hintText!) else SizedBox.shrink(),
+              customArrow ?? Icon(Icons.arrow_drop_down_rounded)
+            ],
           ),
         ).asGestureDetector(
           onTap: isActive == true
